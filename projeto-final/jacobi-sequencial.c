5635cf583e8c440a2fa96ca345928c2adef20ce8
andrecatini <andrecatini@fb0d131d-974c-6bbd-dc42-787147c8dd2b>
2013-07-05 06:44:23 +0000

    versao final, sem comentário

--- projeto-final/jacobi-sequencial.c
+++ projeto-final/jacobi-sequencial.c
@@ -6,7 +6,7 @@
 void printMA(int J_ORDER, float **MA);
 void printMB(int J_ORDER, float *MB);
 void printX(int J_ORDER, float *X);
-void printResults(int ite, int J_ROW_TEST, float result, float *MB);
+void printResults(int ite, int J_ROW_TEST, float result, float MB);
 
 int main(int argc, char * argv[]){
 	
@@ -24,36 +24,31 @@ int main(int argc, char * argv[]){
 	
 	fscanf(pFile, "%d%d%f%f", &J_ORDER, &J_ROW_TEST, &J_ERROR, &J_ITE_MAX);
 	
-	float **MA, *MB, *X;
-	float **MAOriginal, *MBOriginal;
+	float **MA, *MAOriginal, *MB, MBOriginal, *X;	
 	
-	MA = (float**)malloc(sizeof(float*)*J_ORDER);
-	MAOriginal = (float**)malloc(sizeof(float*)*J_ORDER);
-	
-	MB = (float*)malloc(sizeof(float)*J_ORDER);
-	MBOriginal = (float*)malloc(sizeof(float)*J_ORDER);
-	X = (float*)malloc(sizeof(float)*J_ORDER);
+	MA = 		(float**)malloc(sizeof(float*)*J_ORDER);
+	MB = 		(float*)malloc(sizeof(float)*J_ORDER);
+	MAOriginal = 	(float*)malloc(sizeof(float)*J_ORDER);
+	X = 		(float*)malloc(sizeof(float)*J_ORDER);
 		
 	for(i = 0; i<J_ORDER; i++){
 		
 		MA[i] = (float*)malloc(sizeof(float)*J_ORDER);
-		MAOriginal[i] = (float*)malloc(sizeof(float)*J_ORDER);
 		
 		for(j = 0; j<J_ORDER; j++){
 			fscanf(pFile, "%f", &MA[i][j]);
-			MAOriginal[i][j] = MA[i][j];
+			if(i == J_ROW_TEST)
+				MAOriginal[j] = MA[i][j];
 		}
 	}
 		
 	for(i=0; i<J_ORDER; i++){
 		fscanf(pFile, "%f", &MB[i]);
-		MBOriginal[i] = MB[i];
-		
+		if(i == J_ROW_TEST)
+			MBOriginal = MB[i];		
 	}
 	
 	fclose(pFile);
-	
-	printf("LEU");
 	
 	int diagAux;
 	for(i = 0; i<J_ORDER; i++){
@@ -68,56 +63,50 @@ int main(int argc, char * argv[]){
 		
 		MB[i] = MB[i] / diagAux;
 		X[i] = MB[i];
-	}
-	
+	}	
 	
 	int ite=0;
 	float Mr = FLT_MAX;
 
-	float sumAux;
-	float maxDif, maxX, xAux;
-	
+	float sumAux[J_ORDER];
+	float maxDif, maxX, xAux;	
 
 	while((ite<J_ITE_MAX) && (Mr > J_ERROR)){
 		
 		maxDif = maxX = FLT_MIN;
 		
 		for(i = 0; i<J_ORDER; i++){
-			xAux = X[i];
-			sumAux=0;
+			
+			sumAux[i]=0;
 		
 			for(j = 0; j<J_ORDER; j++){
 				if(i!=j)
-					sumAux += (MA[i][j]*X[j]);
-			}
+					sumAux[i] += (MA[i][j]*X[j]);
+			}		
 			
-			X[i] = MB[i] - sumAux;
+		}	
+		
+		for(i = 0; i <J_ORDER; i++){
 			
+			xAux = X[i];
+			X[i] = MB[i] - sumAux[i];
+				
 			if(fabs(X[i] - xAux) > maxDif)
 				maxDif = fabs(X[i] - xAux);
 			
 			if(fabs(X[i]) > maxX)
 				maxX = fabs(X[i]);
-			
-		}
 		
-		Mr = maxDif / maxX;
-		//printf("mr: %f\nmaxDif: %f\tmaxX: %f\n", Mr, maxDif, maxX);
+		}		
 		
-		//printX(J_ORDER, X);
-		
-		ite++;
-		
-		
+		Mr = maxDif / maxX;		
+		ite++;	
 	}
 
-	
 	float resultAux=0;
 	for(j=0; j<J_ORDER; j++){
-		resultAux += MAOriginal[J_ROW_TEST][j]*X[j];
+		resultAux += MAOriginal[j]*X[j];
 	}
-
-
 
 	printResults(ite, J_ROW_TEST, resultAux, MBOriginal);
 	
@@ -125,11 +114,8 @@ int main(int argc, char * argv[]){
 	free(MB);
 	free(X);
 	free(MAOriginal);
-	free(MBOriginal);
 	
-	
-	return 0;
-	
+	return 0;	
 }
 
 	
@@ -160,9 +146,9 @@ void printX(int J_ORDER, float *X){
 	printf("\n");
 }
 
-void printResults(int ite, int J_ROW_TEST, float result, float *MB){
+void printResults(int ite, int J_ROW_TEST, float result, float MB){
 	printf("\n\n---------------------------------------------------------\n"
 	"Iterations: %d\n"
 	"RowTest: %d => [%f] =? %f\n"
-	"---------------------------------------------------------\n\n", ite, J_ROW_TEST, result, MB[J_ROW_TEST]);
+	"---------------------------------------------------------\n\n", ite, J_ROW_TEST, result, MB);
 }
